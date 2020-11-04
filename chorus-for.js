973615bf4971044d0922cec6d622ac4d36e6d92f
Andrew Matthews <andymat1964@gmail.com>
2020-11-04 23:21:20 +0000

    latest updateed files

--- chorus-for.js
+++ chorus-for.js
@@ -1,5 +1,5 @@
 const chorus = "Let's dance!";
-for (repeat =0; repeat < 10; repeat ++) {
+for (repeat = 0; repeat < 10; repeat ++) {
     console.log(chorus);
 }
 console.log("Until the sun comes up!");
